da4578506769d4d74eaec5aedb898e357e1a290a
Magdalena <magdalenapaluch1@gmail.com>
2023-06-17 11:39:32 +0200

    Pylint improvements

--- Zadanie_0_quiz/00_quiz.py
+++ Zadanie_0_quiz/00_quiz.py
@@ -1,3 +1,5 @@
+"""CONSOLE QUIZ"""
+
 import json
 import datetime
 
@@ -5,8 +7,15 @@ from QuestionSet import QuestionSet
 
 SCORE_FILENAME = "results.json"
 
-def SaveScore(name, new_score):
-    with open(SCORE_FILENAME, 'r') as scores_file:
+def save_score(name, new_score):
+    """Save new score to file.
+    Function checks if name is already in results.
+    If name exist - check the result, result is saved only when it's better.
+    Parameters:
+    name(str): user name
+    new_score(int): user points
+    """
+    with open(SCORE_FILENAME, 'r', encoding="utf8") as scores_file:
         current_scores = json.load(scores_file)
 
     name_already_in_list = False
@@ -23,16 +32,20 @@ def SaveScore(name, new_score):
 
     current_scores_sorted = sorted(current_scores, key=lambda d: d['score'], reverse=True)
 
-    with open(SCORE_FILENAME, 'w') as scores_file:
+    with open(SCORE_FILENAME, 'w', encoding="utf8") as scores_file:
         json.dump(current_scores_sorted, scores_file, default=str)
 
 
-def PrintTopScores(number):
-    #wyświetlanie top wyników
+def print_top_scores(number):
+    """Function print TOP given numbers of results.
+    Parameters:
+    number(int): number of printing scores
+    """
+
     score_board_counter = 1
-    with open(SCORE_FILENAME, 'r') as scores_file:
+    with open(SCORE_FILENAME, 'r', encoding="utf8") as scores_file:
         scores_data = json.load(scores_file)
-        print(f"Lista {number} najlepszych wyników: ")
+        print(f" {number} TOP results: ")
 
         for score in scores_data:
             print(f'{score_board_counter}. {score["name"]} {score["score"]}')
@@ -41,12 +54,10 @@ def PrintTopScores(number):
                 break
 
 if __name__ == "__main__":
-    with open('questions.json') as file:
+    with open('questions.json', encoding="utf8") as file:
         quiz_data = json.load(file)
 
-    #print(data)
-    user_score = 0
-    #wyświetlenie pytania i odpowiedzi
+    USER_SCORE = 0
 
     question_list = []
 
@@ -56,19 +67,17 @@ if __name__ == "__main__":
         question_list.append(new_question)
         new_question.PrintQuestion()
         new_question.PrintAnswers()
-        user_answer = input("Podaj poprawną odpowiedź: ")
-        if new_question.CheckAnswer(user_answer) == True:
-            user_score += 1
+        user_answer = input("Right answer: ")
+        if new_question.CheckAnswer(user_answer) is True:
+            USER_SCORE += 1
 
-    #poproszenie użytkownika o podanie imienia
-    user_name = input("Podaj swoje imię: ")
+    user_name = input("Your name: ")
 
-    #wyświetlenie wyniku z podanym imieniem gracza
-    print(f'{user_name}, zdobywasz {user_score} / {len(quiz_data)} pkt!')
+    print(f'{user_name}, you earn {USER_SCORE} / {len(quiz_data)} points!')
 
-    SaveScore(user_name, user_score)
+    save_score(user_name, USER_SCORE)
 
-    PrintTopScores(5)
+    print_top_scores(5)
 
 #TODO
 # klasa user
