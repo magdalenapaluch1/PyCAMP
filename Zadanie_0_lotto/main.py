a50886d318695e558f3952bd4f17f4f6f7ca35fd
Magdalena <magdalenapaluch1@gmail.com>
2023-06-19 20:26:12 +0200

    Counting all wins Price_Of_Coupons added

--- Zadanie_0_lotto/main.py
+++ Zadanie_0_lotto/main.py
@@ -20,19 +20,50 @@ def Random_Lotto_Set():
 def Draw_Until_Win(user_numbers):
     lotto_numbers = set()
     i = 0
+    count_3 = 0
+    count_4 = 0
+    count_5 = 0
     while user_numbers != lotto_numbers:
         lotto_numbers = Random_Lotto_Set()
         i += 1
+        intersection = user_numbers.intersection(lotto_numbers)
         if user_numbers == lotto_numbers:
             print(f"Win! You won after {i} lotteries.")
+        elif len(intersection) == 3:
+            count_3 += 1
+        elif len(intersection) == 4:
+            count_4 += 1
+        elif len(intersection) == 5:
+            count_5 += 1
+
+    print(f"You hit 3 in lottery {count_3} times.")
+    print(f"You hit 4 in lottery {count_4} times.")
+    print(f"You hit 5 in lottery {count_5} times.")
+
+    return i
+
+def Price_Of_Coupons(number_of_coupons):
+    one_coupon_price = 3
+
+    return (one_coupon_price * number_of_coupons)
 
 if __name__ == '__main__':
 
+    print("""
+    This is Lotto simulation.
+    One coupon costs 3PLN.
+    There are 3 lotteries in a week.
+    Let's see an example of playing continuously until win.
+    Good luck!
+    """)
+
     numbers = User_Choose_Numbers()
 
-    Draw_Until_Win(numbers)
+    number_of_coupons = Draw_Until_Win(numbers)
+
+    print(f"You spent: {Price_Of_Coupons(number_of_coupons):,} PLN.")
 
 #TODO
-#Policzyć ile to kosztuje
-#Czy wypadła 5, 4, 3
 #Kalkulacja opłacalności
+#zwrócić wyniki z funkcji Draw_To_Win
+#Określenie wartości wygranych
