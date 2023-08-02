ab552efb5f33fbb59c5b8ebb49821afcf0c1f9c1
Magdalena <magdalenapaluch1@gmail.com>
2023-08-02 18:56:43 +0200

    Multiple reasons for non safe password handled

--- Project_2_password/password_checker.py
+++ Project_2_password/password_checker.py
@@ -1,28 +1,44 @@
-
 class Password_Checker():
 
-    def is_password_safe(self, password) -> bool:
-        len_password = len(password)
-        safe = True
-        if len_password < 8:
-            safe = False
+    def is_password_safe(self, password) -> list:
+ 
+        safe = []
+        if len(password) < 8:
+            safe.append("too_short")
 
-
-        numbers_in_password = any(chr.isdigit() for chr in password)
-        if numbers_in_password is False:
-            safe = False
+        if any(chr.isdigit() for chr in password) is False:
+            safe.append("no_digit")
     
-        special_char_in_password = any(not chr.isalnum() for chr in password)
-        if special_char_in_password is False:
-            safe = False
+        if any(not chr.isalnum() for chr in password) is False:
+            safe.append("no_special")
 
+        if any(chr.isupper() for chr in password) is False:
+            safe.append("no_upper")
         
-        upper_char_in_password = any(chr.isupper() for chr in password)
-        if upper_char_in_password is False:
-            safe = False
-        
-        lower_char_in_password = any(chr.islower() for chr in password)
-        if lower_char_in_password is False:
-            safe = False
+        if any(chr.islower() for chr in password) is False:
+            safe.append("no_lower")
+
+        if len(safe) == 0:
+            safe.append("password_safe")
 
         return safe
+
+    def print_password_results(self, results):
+
+        if "password_safe" in results:
+            print("Your password is safe.")
+
+        if "too_short" in results:
+            print("Your password is too short.")
+
+        if "no_digit" in results:
+            print("Your password does not have digit.")
+
+        if "no_special" in results:
+            print("Your password does not have special character.")
+
+        if "no_upper" in results:
+            print("Your password does not have uppercase character.")
+            
+        if "no_lower" in results:
+            print("Your password does not have lowercase character.")
